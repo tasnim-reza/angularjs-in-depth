90dcd81ccd8c9734facee306f7637412408fae88
Tasnim Reza <reza5619@gmail.com>
2016-03-21 11:57:29 +0600

    add inheritance feature

--- inheritance/inheritance.js
+++ inheritance/inheritance.js
@@ -1,16 +1,59 @@
 /**
  * Created by Reza on 20-3-16.
  */
-function ControllerA(){
-    this.saya = function(){
-        this.msg = "Controller a";
+
+
+function BaseController(){
+    this.base = function(msg){
+        return 'From base ' + msg;
     }
 }
+var baseCtrl = Object.create(BaseController.prototype);
+BaseController.apply(baseCtrl, []);
+
+function ControllerA($scope, servicea){
+    var self = this;
+
+    $scope.saya = function(){
+        $scope.msg = self.base("Controller a");
+    }
+
+    $scope.getName = function(){
+        $scope.msg = servicea.getName();
+    }
+}
+
+ControllerA.prototype = baseCtrl;
+
 app.controller('ctrla', ControllerA);
 
-function ControllerB(){
-    this.sayb = function(){
-        this.msg = "Controller b";
+function ControllerB($scope){
+    var self = this;
+    $scope.sayb = function(){
+        $scope.msg = self.base("Controller b");;
     }
 }
+
+ControllerB.prototype = baseCtrl;
+
 app.controller('ctrlb', ControllerB);
+
+function BaseService(){
+    this.sayFromBase = function(msg){
+        return msg + ' From base service.';
+    }
+}
+
+var baseService = Object.create(BaseService.prototype);
+BaseService.apply(baseService, []);
+
+function ServiceA(){
+    var self = this;
+    this.getName = function(){
+        return self.sayFromBase('Hi, this is Reza,');
+    }
+}
+
+ServiceA.prototype = baseService;
+
+app.service('servicea', ServiceA)
