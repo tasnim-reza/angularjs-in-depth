67c56d296f0c09a1a309859134567367d1afbc64
Tasnim Reza <reza5619@gmail.com>
2016-03-29 13:12:23 +0600

    gulp integration

--- gulpfile.js
+++ gulpfile.js
@@ -5,62 +5,65 @@
 var gulp = require('gulp');
 
 // Include Our Plugins
-var jshint = require('gulp-jshint');
-//var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+var jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
 
-// Lint Task
-gulp.task('lint', function () {
-    return gulp.src('/*/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
+    streamSeries = require('stream-series'),
+    inject = require('gulp-inject');
 
-// Compile Our Sass
-// gulp.task('sass', function() {
-//     return gulp.src('scss/*.scss')
-//         .pipe(sass())
-//         .pipe(gulp.dest('dist/css'));
-// });
 
-// Concatenate & Minify JS
+// Copy vendor files
+var vendorFiles = gulp.src(['node_modules/jquery/dist/jquery.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/angular/angular.min.js'])
+    .pipe(gulp.dest('dist/vendor'));
+
+// Concatenate & Minify application js files
+var appFile = gulp.src(['src/**/*.js'])
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(rename('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+
 gulp.task('scripts', function () {
-
-    gulp.src(['src/**/*.js'])
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-
-    // Copy vendor files
-    gulp.src(['node_modules/jquery/dist/jquery.min.js',
-            'node_modules/bootstrap/dist/js/bootstrap.min.js',
-            'node_modules/angular/angular.min.js'])
-        .pipe(gulp.dest('dist/js/vendor'));
-
+    appFile, vendorFiles
 });
 
+
+var appCss = gulp.src('src/css/**')
+    .pipe(gulp.dest('dist/css'));
+
+var vendorCss = gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css')
+    .pipe(gulp.dest('dist/vendor'));
 
 // Copy all static assets
 gulp.task('copy', function () {
-    gulp.src('src/site/**')
-        .pipe(gulp.dest('dist/site'));
+    appCss;
+    vendorCss;
 
-    // gulp.src('client/css/**')
-    //     .pipe(gulp.dest('build/css'));
+    gulp.src('src/img/**')
+        .pipe(gulp.dest('dist/img'));
 
     gulp.src('src/**/*.html')
         .pipe(gulp.dest('dist'));
 });
 
+gulp.task('inject', function () {
+    gulp.src('dist/index.html')
+        .pipe(inject(streamSeries(vendorFiles, appFile, vendorCss, appCss), {relative: true}))
+        .pipe(gulp.dest('dist'));
+})
 
 // Watch Files For Changes
 gulp.task('watch', function () {
     gulp.watch('js/*.js', ['lint', 'scripts']);
 });
+
+//build task
+gulp.task('build', ['scripts', 'copy']);
 
 // Default Task
 gulp.task('default', ['lint', 'scripts', 'watch']);
