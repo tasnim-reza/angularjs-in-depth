2ee702c97ded87a8d6b7abeb8bac5ac0099d5514
thmehorizon <thmeshoot@gmail.com>
2016-04-03 23:33:34 +0600

    refactor

--- src/app.js
+++ src/app.js
@@ -1,38 +1,61 @@
 /**
  * Created by Reza on 20-3-16.
  */
+//ToDo: static data should moved to json file
+var routeData ={
+    "/":{
+        "templateUrl": "views/inheritance/inheritance.html",
+            "date": "01 January, 2016"
+    },
+    "/script-in-template":{
+        "templateUrl": "views/script-in-template/script-inside-template.html",
+            "date": "17 February, 2016"
+    },
+    "/routechange-on-scroll":{
+        "templateUrl": "views/routechange-on-scroll/routechange-on-scroll.html",
+            "date": "20 March, 2016"
+    },
+    "/npm-build-combo":{
+        "templateUrl": "views/npm-build-combo.html",
+            "date": "01 April, 2016"
+    }
+};
 var app = angular.module('angularInDepth', ['ngRoute']);
 
 app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/', {
-            templateUrl: 'views/inheritance/inheritance.html',
-            // controller: 'BookController',
-            // resolve: {
-            //     // I will cause a 1 second delay
-            //     delay: function ($q, $timeout) {
-            //         var delay = $q.defer();
-            //         $timeout(delay.resolve, 1000);
-            //         return delay.promise;
-            //     }
-            // }
+    var keys = Object.keys(routeData);
+    keys.forEach(function(key){
+        $routeProvider.when(key, {
+            templateUrl: routeData[key].templateUrl
+        });
+    });
 
-        })
-        .when('/script-in-template', {
-            templateUrl: 'views/script-in-template/script-inside-template.html'
-        })
-        .when('/routechange-on-scroll', {
-            templateUrl: 'views/routechange-on-scroll/routechange-on-scroll.html'
-        })
-        .when('/npm-build-combo', {
-            templateUrl: 'views/npm-build-combo.html'
-        })
+    // $routeProvider.when('/', {
+    //         templateUrl: 'views/inheritance/inheritance.html',
+    //         // controller: 'BookController',
+    //         // resolve: {
+    //         //     // I will cause a 1 second delay
+    //         //     delay: function ($q, $timeout) {
+    //         //         var delay = $q.defer();
+    //         //         $timeout(delay.resolve, 1000);
+    //         //         return delay.promise;
+    //         //     }
+    //         // }
+    //
+    //     })
 
     $routeProvider.otherwise('/')
+}]);
+
+app.run(['$rootScope', '$location', function($rootScope, $location){
+    $rootScope.routeData = routeData;
+    
 }])
 
-app.controller('BlogContainerCtrl', ['$scope', '$route', function ($scope, $route) {
-    $scope.templateUrl = 'views/inheritance/inheritance.html';
-
+app.controller('BlogContainerCtrl', ['$scope', '$route', '$location', function ($scope, $route, $location) {
+    $scope.location={
+        url: $location.url()
+    };
 
 }])
 app.directive('routeChangeOnScroll', ['$route', '$location', function ($route, $location) {
